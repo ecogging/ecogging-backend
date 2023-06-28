31b617a2b47fd04ff475a3ed82ba7dfc3dd7743f
kmkim2051 <qaws2051@gmail.com>
2023-06-28 16:54:08 +0900

    feat: BaseEntity 추가
    Related to #1

---
+++ src/main/java/com/pickupluck/ecogging/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.pickupluck.ecogging.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+}
