62967ec6ccda0df9cd73a3749d0242d34e0b07a1
kmkim2051 <qaws2051@gmail.com>
2023-06-30 15:40:41 +0900

    chore: 필요 없는 import 제거

--- src/main/java/com/pickupluck/ecogging/domain/user/entity/User.java
+++ src/main/java/com/pickupluck/ecogging/domain/user/entity/User.java
@@ -2,8 +2,6 @@ package com.pickupluck.ecogging.domain.user.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 import lombok.*;
 
 import com.pickupluck.ecogging.domain.BaseEntity;
