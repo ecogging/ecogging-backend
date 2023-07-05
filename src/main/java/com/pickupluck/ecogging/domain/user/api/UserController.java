bceea9461b827b5696a6b91f905d17198d574501
Kyungmin Kim <qaws2051@gmail.com>
2023-07-05 09:43:00 +0900

    feat: 로그인 기능(추후 구현)을 위한 Security 관련 기능 추가

---
+++ src/main/java/com/pickupluck/ecogging/domain/user/api/UserController.java
@@ -0,0 +1,19 @@
+package com.pickupluck.ecogging.domain.user.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * mypage
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping("/")
+    public String tempMain() {
+        return "hello";
+    }
+}
