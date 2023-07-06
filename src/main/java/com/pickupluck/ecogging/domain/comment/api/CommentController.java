2db5d5452494c2e61d97daa949a97a701d7f814e
Kyungmin Kim <qaws2051@gmail.com>
2023-07-06 15:33:09 +0900

    feat: Comment Repository, Service, Controller 틀 추가

---
+++ src/main/java/com/pickupluck/ecogging/domain/comment/api/CommentController.java
@@ -0,0 +1,11 @@
+package com.pickupluck.ecogging.domain.comment.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentController commentController;
+}
---
+++ src/main/java/com/pickupluck/ecogging/domain/comment/service/CommentService.java
@@ -0,0 +1,4 @@
+package com.pickupluck.ecogging.domain.comment.service;
+
+public interface CommentService {
+}
