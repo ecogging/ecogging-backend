fc3eb1e05ab91a970147f5c5720260ea20fd000d
Kyungmin Kim <qaws2051@gmail.com>
2023-07-11 03:34:09 +0900

    feat: security config 설정

--- src/main/java/com/pickupluck/ecogging/config/WebSecurityConfig.java
+++ src/main/java/com/pickupluck/ecogging/config/WebSecurityConfig.java
@@ -2,7 +2,7 @@ package com.pickupluck.ecogging.config;
 
 import com.pickupluck.ecogging.util.jwt.*;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.filters.CorsFilter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -11,14 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
