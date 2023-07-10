94cc288a6832ffedb8c5ccc7f9aefb5892b4b87a
Kyungmin Kim <qaws2051@gmail.com>
2023-07-10 17:01:51 +0900

    feat: JWT Filter 추가

---
+++ src/main/java/com/pickupluck/ecogging/util/jwt/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.pickupluck.ecogging.util.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends GenericFilterBean {
+
+    public static String AUTHORIZATION_HEADER = "Authorization";
+
+    private final TokenProvider tokenProvider;
+
+    // JWT 인증을 위한 필터 구현
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!(request instanceof HttpServletRequest))
+            throw new ServletException("request is not instance of HttpServletRequest");
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+
+        String jwt = resolveToken(httpServletRequest);
+        String requestUri = httpServletRequest.getRequestURI();
+
+        if (StringUtils.hasText(jwt) && tokenProvider.isValidToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("register Authentication '{}' to Security Context. URI: {}", authentication.getName(), requestUri);
+        } else {
+            log.info("No valid JWT. URI: {}", requestUri);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    // request로부터 JWT(string) 추출
+    private String resolveToken(HttpServletRequest request) {
+        final String jwtPrefix = "Bearer ";
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(jwtPrefix)) {
+            return bearerToken.substring(jwtPrefix.length());
+        }
+
+        return null;
+    }
+}
