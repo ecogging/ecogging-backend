c61a211345332ba04c239ed6837373f2fa081f8b
jchoi <janechoi0928@gmail.com>
2023-07-13 14:45:31 +0900

    Feat: 스크랩&참여 기능 추가, 게시판 수정&삭제 기능 추가/구현, 데이터커넥션 테스트

--- src/main/java/com/pickupluck/ecogging/domain/plogging/service/AccompanyService.java
+++ src/main/java/com/pickupluck/ecogging/domain/plogging/service/AccompanyService.java
@@ -1,16 +1,22 @@
 package com.pickupluck.ecogging.domain.plogging.service;
-
-<<<<<<< HEAD
-public interface AccompanyService {
-=======
 import com.pickupluck.ecogging.domain.plogging.dto.AccompanyDTO;
+import com.pickupluck.ecogging.domain.plogging.entity.Accompany;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface AccompanyService {
 
     Map<String, Object> getAccompanyList(Integer page, String orderby) throws Exception;
     void setAccompany(Integer temp, AccompanyDTO accompanyDTO) throws Exception;
->>>>>>> accomp-connect
+    AccompanyDTO getAccompany(Long id) throws Exception;
+
+    void removeAccompany(Long id) throws  Exception;
+
+    Boolean isParticipation(Long userId, Long accompanyId) throws Exception;
+    Boolean toggleParticipation(Long userId, Long accompanyId) throws Exception;
+
+    Boolean isAccompanyScrap(Long userId, Long accompanyId) throws Exception;
+    Boolean toggleAccompanyScrap(Long userId, Long accompanyId) throws Exception;
 }
--- src/test/java/com/pickupluck/ecogging/EcoggingApplicationTests.java
+++ src/test/java/com/pickupluck/ecogging/EcoggingApplicationTests.java
@@ -1,12 +1,17 @@
 package com.pickupluck.ecogging;
 
+import com.pickupluck.ecogging.domain.plogging.repository.AccompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcoggingApplicationTests {
-
-	void contextLoads() {
-
-	}
+//	@Autowired
+//	AccompanyRepository repository;
+//	@Test
+//	void contextLoads() {
+//		System.out.println(repository.findAll());
+//	}
 
 }
