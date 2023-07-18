47ebc114c7e5c12f0988aba9d91a38de542ff4b4
donguri <holymolyguri@gmail.com>
2023-07-19 02:02:03 +0900

    Feat: make Message Repository MessageContentRepository, MessageRepository, MessageContentRepository 생성

---
+++ src/main/java/com/pickupluck/ecogging/domain/message/repository/MessageContentRepository.java
@@ -0,0 +1,7 @@
+package com.pickupluck.ecogging.domain.message.repository;
+
+import com.pickupluck.ecogging.domain.message.entity.MessageContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageContentRepository extends JpaRepository<MessageContent, Long> {
+}
---
+++ src/main/java/com/pickupluck/ecogging/domain/message/repository/MessageRoomRepository.java
@@ -0,0 +1,7 @@
+package com.pickupluck.ecogging.domain.message.repository;
+
+import com.pickupluck.ecogging.domain.message.entity.MessageRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRoomRepository extends JpaRepository<MessageRoom, Long> {
+}
