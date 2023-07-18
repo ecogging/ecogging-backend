d78aea5535f59cc22371a6771e9b6fb13bc7fd13
donguri <holymolyguri@gmail.com>
2023-07-19 03:10:57 +0900

    Feat: Message Entity 간 연관 관계 추가 MessageRoom - Message 간 FK 참조 누락 수정 Message - MessageContent 간 FK 참조 누락 수정

--- src/main/java/com/pickupluck/ecogging/domain/message/repository/MessageContentRepository.java
+++ src/main/java/com/pickupluck/ecogging/domain/message/repository/MessageContentRepository.java
@@ -1,7 +1,8 @@
 package com.pickupluck.ecogging.domain.message.repository;
 
+import com.pickupluck.ecogging.domain.message.entity.Message;
 import com.pickupluck.ecogging.domain.message.entity.MessageContent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageContentRepository extends JpaRepository<MessageContent, Long> {
+public interface MessageContentRepository extends JpaRepository<MessageContent, Message> {
 }
