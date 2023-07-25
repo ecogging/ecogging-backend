e9347cf5ff7e4952339a044a47b6d6e6ece947a4
jchoi <janechoi0928@gmail.com>
2023-07-25 16:19:16 +0900

    Refactor: MyPage 플로깅 리뷰 관련 Query, 충돌오류누락 수정

--- src/main/java/com/pickupluck/ecogging/domain/plogging/api/AccompanyController.java
+++ src/main/java/com/pickupluck/ecogging/domain/plogging/api/AccompanyController.java
@@ -8,6 +8,7 @@ import com.pickupluck.ecogging.domain.notification.service.NotificationService;
 import com.pickupluck.ecogging.domain.plogging.dto.AccompanyDTO;
 import com.pickupluck.ecogging.domain.plogging.service.AccompanyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
