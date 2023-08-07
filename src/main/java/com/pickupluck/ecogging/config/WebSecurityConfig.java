8cbc763393eb5480afaa684737ff0e7ca584f8c4
Kyungmin Kim <qaws2051@gmail.com>
2023-08-08 00:22:19 +0900

    fix: 누락된 redirection endpoint 추가

--- src/main/java/com/pickupluck/ecogging/config/WebSecurityConfig.java
+++ src/main/java/com/pickupluck/ecogging/config/WebSecurityConfig.java
@@ -1,23 +1,24 @@
 package com.pickupluck.ecogging.config;
 
-import com.pickupluck.ecogging.domain.user.service.UserOAuthService;
-import com.pickupluck.ecogging.util.jwt.*;
-import com.pickupluck.ecogging.util.oauth.OAuth2AuthenticationSuccessHandler;
-import lombok.RequiredArgsConstructor;
-
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+import com.pickupluck.ecogging.domain.user.service.UserOAuthService;
+import com.pickupluck.ecogging.util.jwt.*;
+import com.pickupluck.ecogging.util.oauth.OAuth2AuthenticationSuccessHandler;
+
 
 @Configuration
 @EnableWebSecurity(debug = false)
@@ -36,7 +37,7 @@ public class WebSecurityConfig {
     private final UserOAuthService userOauthService;
 
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -59,10 +60,13 @@ public class WebSecurityConfig {
 
                 .oauth2Login((oauth2Login) -> oauth2Login
                         .defaultSuccessUrl("/")
-                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                        .redirectionEndpoint((redirectionEndpointConfig ->
+                            redirectionEndpointConfig.baseUri(("/auth/oauth2/kakao"))
+                        ))
                         .userInfoEndpoint(userInfo ->
                             userInfo.userService(userOauthService)
                         )
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
                 )
                 .apply(new JwtSecurityConfig(tokenProvider));
 
