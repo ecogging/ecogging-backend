a19babee89a6e2ca59ccb25693697b7164bd7862
Kyungmin Kim <qaws2051@gmail.com>
2023-08-08 00:28:27 +0900

    feat: userId 포함하기 위한 OAuthUser 클래스

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into user(user_id, nickname, email, password) values(1, "admin", "admin@test.com", "1234");
 insert into user(user_id, nickname, email, password) values(2, "test", "test@test.com", "1234");
 insert into user(user_id, nickname, email, password) values(3, "test2", "test2@test.com", "1234");
-insert into user(user_id, nickname, email, password, telephone, profile_image_url) values(4, "kim", "qaws2051@naver.com", "123123123", "01012341234", "https://ecogging-kosta.s3.ap-northeast-2.amazonaws.com/profile/profile-default.png");
+insert into user(user_id, nickname, email, password, telephone, profile_image_url) values(4, "kim", "qaws2051@test.com", "123123123", "01012341234", "https://ecogging-kosta.s3.ap-northeast-2.amazonaws.com/profile/profile-default.png");
 
 
 insert into notification(notification_id, target_id, sender_id, receiver_id, type, detail, is_read) values(1, 1, 1, 2, "COMMENT","글제목~", 0);
